4f2de221e73177e419f49015acc08aa0005c8c6b
baduker <baduker@protonmail.ch>
2019-09-10 10:27:06 +0200

    Fixed html file count.

--- scripts/rm_html.sh
+++ scripts/rm_html.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-FIND_HTML=$(find . -maxdepth 1 -type f -name "*.html")
-HTML_FILE_COUNT=$(echo $FIND_HTML | wc -l)
+HTML_FILE_COUNT=$(find . -maxdepth 1 -type f -name "*.html" | wc -l)
 DELETE_HTML=$(find . -maxdepth 1 -type f -name "*.html" -delete)
 
 echo -n "$DELETE_HTML"
